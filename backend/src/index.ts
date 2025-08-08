06f4abb9ea3cad141431f24d3e88a948d93dc3a8
Manish Modanwal <manishmodanwal400@gmail.com>
2025-08-09 01:59:46 +0530

    updated package

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-     origin: 'https://testify-ai-sage.vercel.app', 
+    origin: process.env.CLIENT_URL, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
